084b499360a4ad894e2599843f4f763abfdb9c73
zamorano <bzam1204@gmail.com>
2024-12-07 23:02:18 -0300

    feat: config axiosInstance.ts para ambos os projetos

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production'
+  ? process.env.NEXT_PUBLIC_API_BASE_URL
+  : 'http://localhost:3000/api';
+
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
