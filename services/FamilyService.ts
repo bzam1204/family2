e9d97d6d17b57082822037d0d8a512305c886184
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2025-02-18 20:12:29 -0300

    refactor: improve formatting and consistency in FamilyService methods

--- services/FamilyService.ts
+++ services/FamilyService.ts
@@ -8,127 +8,133 @@ import { CreateFamilyDto } from "@/lib/dto/CreateFamilyDto";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 interface IFamilyService {
-  get( id : string ) : Promise<Family>;
+    get(id: string): Promise<Family>;
 
-  getAll( kindeId : string, includeUser : true ) : Promise<( Family & { user : User } ) []>;
+    getAll(
+        kindeId: string,
+        includeUser: true
+    ): Promise<(Family & { user: User })[]>;
 
-  getAll( kindeId : string, includeUser ? : false ) : Promise<Family []>;
+    getAll(kindeId: string, includeUser?: false): Promise<Family[]>;
 
-  create( data : CreateFamilyDto ) : Promise<Family>;
+    create(data: CreateFamilyDto): Promise<Family>;
 
-  remove( id : string ) : Promise<Family>;
+    remove(id: string): Promise<Family>;
 
-  generateShoppingList( familyId : string ) : Promise<{ name : string, shopQuantity : number }[]>;
+    generateShoppingList(
+        familyId: string
+    ): Promise<{ name: string; shopQuantity: number }[]>;
 }
 
-export const FamilyService : IFamilyService = {
-  get : async function ( id ) {
-    const family = await db.family.findUnique( { where : { id } } );
+export const FamilyService: IFamilyService = {
+    get: async function (id) {
+        const family = await db.family.findUnique({ where: { id } });
 
-    if ( !family ) throw new Error( "Not Found" );
+        if (!family) throw new Error("Not Found");
 
-    return family;
-  },
-  getAll : async ( kindeId, includeUser = false ) => await db.family.findMany( {
-    where : {
-      isActive : true,
-      families : { some : { user : { kindeId } } },
+        return family;
     },
-    include : {
-      user : includeUser
-    }
+    getAll: async (kindeId, includeUser = false) =>
+        await db.family.findMany({
+            where: {
+                isActive: true,
+                families: { some: { user: { kindeId } } },
+            },
+            include: {
+                user: includeUser,
+            },
+        }),
+    create: async function (data) {
+        console.log({ fn: this.create, data });
+        const { getUser } = getKindeServerSession();
+        const kindeUser = await getUser();
 
-  } ),
-  create : async function ( data ) {
-    console.log( { fn : this.create, data } );
-    const { getUser } = getKindeServerSession();
-    const kindeUser = await getUser();
+        if (!kindeUser) throw new Error("Usuário não existe");
 
-    if ( !kindeUser ) throw new Error( "Usuário não existe" );
+        const user = await db.user.findFirst({
+            where: {
+                kindeId: kindeUser.id,
+            },
+        });
 
-    const user = await db.user.findFirst( {
-      where : {
-        kindeId : kindeUser.id,
-      },
-    } );
+        if (!user) throw new Error("Something Went Wrong");
 
-    if ( !user ) throw new Error( "Something Went Wrong" );
+        const family = await db.family.create({
+            data: {
+                name: data.name,
+                creatorUserId: user.id,
+            },
+        });
 
-    const family = await db.family.create( {
-      data : {
-        name : data.name,
-        creatorUserId : user.id,
-      },
-    } );
+        if (!family) throw new Error("Something Went Wrong");
 
-    if ( !family ) throw new Error( "Something Went Wrong" );
+        await db.familyUser.create({
+            data: {
+                role: FamilyUserRole.ADMIN,
+                userId: user.id,
+                familyId: family.id,
+            },
+        });
 
-    await db.familyUser.create( {
-      data : {
-        role : FamilyUserRole.ADMIN,
-        userId : user.id,
-        familyId : family.id,
-      },
-    } );
+        revalidatePath("/dashboard/family");
 
-    revalidatePath( "/dashboard/family" );
+        return family;
+    },
+    remove: async function (id) {
+        console.log({ id });
+        return await db.family.update({
+            where: {
+                id,
+            },
+            data: {
+                isActive: false,
+                families: {
+                    updateMany: {
+                        where: { familyId: id },
+                        data: { isActive: false },
+                    },
+                },
+                InventoryItem: {
+                    updateMany: {
+                        where: { familyId: id },
+                        data: { isActive: false },
+                    },
+                },
+            },
+        });
+    },
+    generateShoppingList: async function (familyId) {
+        // const items = await db.inventoryItem.findMany( {
+        //   where : {
+        //     familyId,
+        //     idealQuantity : {
+        //       not : null,
+        //     },
+        //     quantity : {
+        //       lt : { idealQuantity : true },
+        //       gte : { idealQuantity : true, substract : true },
+        //     }
+        //   }
+        // } );
 
-    return family;
-  },
-  remove : async function ( id ) {
-    console.log( { id } );
-    return await db.family.update( {
-      where : {
-        id,
-      },
-      data : {
-        isActive : false,
-        families : {
-          updateMany : {
-            where : { familyId : id },
-            data : { isActive : false },
-          },
-        },
-        InventoryItem : {
-          updateMany : {
-            where : { familyId : id },
-            data : { isActive : false },
-          },
-        },
-      },
-    } );
-  },
-  generateShoppingList : async function ( familyId ) {
-    // const items = await db.inventoryItem.findMany( {
-    //   where : {
-    //     familyId,
-    //     idealQuantity : {
-    //       not : null,
-    //     },
-    //     quantity : {
-    //       lt : { idealQuantity : true },
-    //       gte : { idealQuantity : true, substract : true },
-    //     }
-    //   }
-    // } );
-
-    const items = await db.$queryRaw<InventoryItem[]>`
+        const items = await db.$queryRaw<InventoryItem[]>`
         SELECT *
         FROM "InventoryItem"
-        WHERE "familyId" = ${ familyId }::uuid
+        WHERE "familyId" = ${familyId}::uuid
           AND "quantity" < "idealQuantity"
+          AND "isActive" = true
           AND "idealQuantity" IS NOT NULL;
     `;
 
-    console.log( { items } );
+        console.log({ items });
 
-    if ( !items ) {
-      throw new Error( "Something Went Wrong" );
-    }
+        if (!items) {
+            throw new Error("Something Went Wrong");
+        }
 
-    return items.map( i => ( {
-      name : i.name,
-      shopQuantity : i.idealQuantity! - i.quantity,
-    } ) );
-  }
+        return items.map((i) => ({
+            name: i.name,
+            shopQuantity: i.idealQuantity! - i.quantity,
+        }));
+    },
 };
